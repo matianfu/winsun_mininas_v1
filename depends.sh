7a2c235caee239f5e08528cd8856935e26e0abc5
matianfu <matianfu@gmail.com>
2015-12-04 18:37:50 +0800

    modified preceed, add finalize

--- depends.sh
+++ depends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install kernel-package expect tree \
+sudo apt-get -y install kernel-package expect tree \
 	build-essential gawk alien fakeroot \
 	zlib1g-dev uuid-dev libblkid-dev libselinux-dev parted lsscsi wget
