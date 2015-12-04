398fb69c488b6cbf97acbb8d285db20623e89056
matianfu <matianfu@gmail.com>
2015-12-05 00:48:37 +0800

    merge inventec patches

--- depends.sh
+++ depends.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get -y install kernel-package expect tree \
 	build-essential gawk alien fakeroot \
-	zlib1g-dev uuid-dev libblkid-dev libselinux-dev parted lsscsi wget
+	zlib1g-dev uuid-dev libblkid-dev libselinux-dev parted lsscsi wget \
+	libncurses5 libncurses5-dev bison flex doxygen
