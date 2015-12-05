dfd0cfb8a23f6e9fb5cf8168bef20ca843f9c5be
matianfu <matianfu@gmail.com>
2015-12-05 15:59:35 +0800

    minor fix for build_deb_packages

---
+++ depends_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get -y install kernel-package expect tree \
+	build-essential gawk alien fakeroot \
+	zlib1g-dev uuid-dev libblkid-dev libselinux-dev parted lsscsi wget \
+	libncurses5 libncurses5-dev bison flex doxygen pkg-config
